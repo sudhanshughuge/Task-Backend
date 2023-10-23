6daa4368154611aef743fcedd5925028bac76630
sudhanshughuge <sudhanshughuge@gmail.com>
2023-10-23 21:07:56 +0530

    first commit

---
+++ src/main/java/com/task/TaskBackend/TaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.task.TaskBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBackendApplication.class, args);
+	}
+
+}
